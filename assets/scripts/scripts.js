dda4071ffb155a5a1602438607ef05ee56831545
rodnei0 <rodnei0@gmail.com>
2021-11-05 15:37:42 -0400

    inserindo em ordem aleatoria

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,23 +1,32 @@
 let quantasCartas;
 
-while (quantasCartas % 2 !== 0){
-    quantasCartas = prompt("Com quantas cartas você quer jogar?");
-    if (quantasCartas % 2 !== 0) {
-        alert("Digite somente números pares");
-    }
+while (quantasCartas % 2 !== 0) {
+  quantasCartas = prompt("Com quantas cartas você quer jogar?");
+  if (quantasCartas % 2 !== 0) {
+    alert("Digite somente números pares");
+  }
 }
+
+let arrayDeCartas = [];
 
 const inserirCartas = document.querySelector(".mesaJogo");
 
 for (let i = 0; i < quantasCartas; i++) {
-    inserirCartas.innerHTML += `
+  arrayDeCartas[i] = `
         <div class="card">
             <div class="front-face face">
                 <img src="assets/imagens/parot.png" alt="parrot">
             </div>
             <div class="back-face face">
-                Verso
+                Verso${i}
             </div>
-        </div>
-    `
+        </div>`;
+}
+
+arrayDeCartas.sort(comparador);
+
+inserirCartas.innerHTML = arrayDeCartas.join(" ");
+
+function comparador() {
+  return Math.random() - 0.5;
 }
