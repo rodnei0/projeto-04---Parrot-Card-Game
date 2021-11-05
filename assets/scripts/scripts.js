9f37e9746ef4f1f3d92b3ae893846233b0112184
rodnei0 <rodnei0@gmail.com>
2021-11-05 15:49:27 -0400

    inserindo em ordem aleatoria revisado

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -13,19 +13,24 @@ const inserirCartas = document.querySelector(".mesaJogo");
 
 for (let i = 0; i < quantasCartas; i++) {
   arrayDeCartas[i] = `
-        <div class="card">
-            <div class="front-face face">
-                <img src="assets/imagens/parot.png" alt="parrot">
-            </div>
-            <div class="back-face face">
-                Verso${i}
-            </div>
-        </div>`;
+    <div class="card" onclick="virarCarta(this)">
+        <div class="front-face face">
+            <img src="assets/imagens/parot.png" alt="parrot">
+        </div>
+        <div class="back-face face">
+            Verso${i}
+        </div>
+    </div>`;
 }
 
 arrayDeCartas.sort(comparador);
 
 inserirCartas.innerHTML = arrayDeCartas.join(" ");
+
+// function virarCarta(elemento) {
+//     elemento.classList.add(".click")
+
+// }
 
 function comparador() {
   return Math.random() - 0.5;
