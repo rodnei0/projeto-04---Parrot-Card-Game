d551561d5cc2e06cf799e3284310ca36464b625d
rodnei0 <rodnei0@gmail.com>
2021-11-05 21:55:46 -0400

    jogo funcionando, faltando validacoes e mensagem final

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,30 +7,65 @@ while (quantasCartas % 2 !== 0 || quantasCartas < 4 || quantasCartas > 14) {
   }
 }
 
-let arrayDeCartas = [];
 
-const inserirCartas = document.querySelector(".mesaJogo");
+let arrayDeCartas = [];
+quantasCartas = quantasCartas/2;
 
 for (let i = 0; i < quantasCartas; i++) {
   arrayDeCartas[i] = `
-    <div class="card" onclick="virarCarta(this)">
-        <div class="front-face face">
-            <img src="assets/imagens/parot.png" alt="parrot">
-        </div>
-        <div class="back-face face">
-            Verso${i}
-        </div>
-    </div>`;
+  <div class="card " data-identifier="card" onclick="virarCarta(this, ${i})">
+    <div class="front-face face" data-identifier="front-face">
+      <img src="assets/imagens/parot.png" alt="parrot">
+    </div>
+    <div class="back-face face" data-identifier="back-face">
+      <img src="assets/imagens/parot${i}.gif" alt="parrot">
+    </div>
+  </div>`
+  ;
+}
+
+for (let i = 0; i < quantasCartas; i++) { 
+  arrayDeCartas.push(arrayDeCartas[i]);
 }
 
 arrayDeCartas.sort(comparador);
 
+const inserirCartas = document.querySelector(".mesaJogo");
+
 inserirCartas.innerHTML = arrayDeCartas.join(" ");
 
-// function virarCarta(elemento) {
-//     elemento.classList.add(".click")
+let teste = [];
+let teste2 = [];
 
-// }
+function virarCarta(elemento, par) {
+    teste2.push(elemento);
+    teste.push(par);
+
+    elemento.classList.toggle("click")
+    setTimeout(comparaCarta, 1000);
+}
+
+function desvirarCarta(elemento){
+  elemento.classList.toggle("click")
+}
+
+function comparaCarta() {
+  if (teste.length === 2) {
+    if (teste[0] === teste [1]) {
+      const elemento = document.querySelectorAll(".click");
+      for (let i = 0; i < 2; i++) {
+        elemento[i].classList.add("acertou");
+        elemento[i].classList.remove("click");
+      }
+    } else {
+        for (let i = 0; i < 2; i++) {
+          desvirarCarta(teste2[i]);
+        }
+      } 
+    teste = [];
+    teste2 = [];
+    }
+} 
 
 function comparador() {
   return Math.random() - 0.5;
